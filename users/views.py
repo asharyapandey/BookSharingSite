f514de3de6238f2987253bcf2a0ed2014fe18265
asharyapandey <asharyap7@gmail.com>
2020-01-02 18:16:33 +0545

    added modal for login and register .and configured routes for register

--- users/views.py
+++ users/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        print(request.POST['username'])
+
+    return render(request, 'users/profile.html')
