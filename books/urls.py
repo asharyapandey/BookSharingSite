cc5af7fa0917775a30c6b539aa8b55dae5fae710
asharyapandey <asharyap7@gmail.com>
2020-01-28 20:52:13 +0545

    CRUD is complete

--- books/urls.py
+++ books/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
-from .views import BookListView, BookDetailView, BookCreateView, BookUpdateView
+from .views import BookListView, BookDetailView, BookCreateView, BookUpdateView, BookDeleteView
 
 from . import views
 
 urlpatterns = [
     path('', BookListView.as_view(), name='books-home'),
     path('book/<int:pk>/', BookDetailView.as_view(), name='books-detail'),
-    path('add/', BookCreateView.as_view(), name='books-create'),
+    path('add/', BookCreateView.as_view(), name='books-add'),
     path('book/<int:pk>/update/', BookUpdateView.as_view(), name='books-update'),
+    path('book/<int:pk>/delete/', BookDeleteView.as_view(), name='books-delete'),
 ]
 
 
--- books/views.py
+++ books/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import BookDetails
 
 
@@ -10,10 +11,12 @@ def books(request):
 class BookListView(ListView):
     model = BookDetails
     context_object_name = 'books'
+    template_name = 'books/books.html'
 
 class BookDetailView(DetailView):
     model = BookDetails
     context_object_name = 'book'
+    template_name = 'books/books_detail.html'
 
 class BookCreateView(LoginRequiredMixin, CreateView):
     model = BookDetails
@@ -23,10 +26,27 @@ class BookCreateView(LoginRequiredMixin, CreateView):
         form.instance.added_by = self.request.user
         return super().form_valid(form)
 
-class BookUpdateView(LoginRequiredMixin, UpdateView):
+class BookUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = BookDetails
     fields= ['name', 'author', 'publisher', 'pages', 'published_year', 'isbn', 'category', 'description', 'image']
 
     def form_valid(self, form):
         form.instance.added_by = self.request.user
         return super().form_valid(form)
+
+    def test_func(self):
+        book = self.get_object()
+        if self.request.user == book.added_by:
+            return True
+        return False
+
+class BookDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = BookDetails
+    context_object_name = 'book'
+    success_url = reverse_lazy('books-home')
+
+    def test_func(self):
+        book = self.get_object()
+        if self.request.user == book.added_by:
+            return True
+        return False
